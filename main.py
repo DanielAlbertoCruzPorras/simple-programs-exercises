1b9c8da77a3fe2c59a26e90e34d809301b704157
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-10-31 11:21:23 -0500

    Ejercicio en clase: Modificación float para fracciones

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 b = 1
 while b == 1:
-    num = int(input("Inserte un número par mayor a 10: "))
+    num = float(input("Inserte un número par mayor a 10: "))
     if num > 10:
         if num % 2 == 0:
             b = 0
         else:
-            print("El número es mayor que 10 pero es impar, inserte otro número que cumpla las condiciones")
+            print("El número es mayor que 10 pero es impar o fraccionario, inserte otro número que cumpla las condiciones")
     else:
         print("El número es menor a 10, inserte otro que cumpla las condiciones")
