c356f8cad5317b7d4952d05221a232b1c80f98a0
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-10-31 12:32:29 -0500

    Ejercicio 14: Palabra más larga

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-dividend = float(input("Insert the dividend: "))
-divisor = float(input("Insert the divisor: "))
-div = dividend/divisor
-quot = div - (div % 1)
-rest = (div % 1)* divisor
-if div % 1 == 0:
-    print(f"the division is exact\nQuotient: {quot:.0f}\nRest: {rest:.0f}")
+firstWord = input("Insert  a word: ")
+secondWord = input("Insert another word: ")
+len1 = len(firstWord)
+len2 = len(secondWord)
+if len1>len2:
+    num = len1-len2
+    print(f"""The word "{firstWord}" has {num} letters more than "{secondWord}" """)
+elif len2>len1:
+    num = len2-len1
+    print(f"""The word "{secondWord}" has {num} letters more than "{secondWord}" """)
 else:
-    print(f"the division is not exact\nQuotient: {quot:.0f}\nRest: {rest:.0f}")
+    print(f"""The word "{firstWord}" is as large as "{secondWord}" """)
