a82f4dc38ada1c7e144cc6eb8eea487f602efe71
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-10-31 11:57:01 -0500

    Ejercicio 12: Año bisiesto o no?

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-num = int(input("Insert a number: "))
-if num % 2 == 0:
-    print("Your number is even")
+year = int(input("Insert a year: "))
+if year < 1582:
+    if year % 4 == 0:
+        print("That year is a leap year")
+    else:
+        print("That year is'nt a leap year")
+elif year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("That year is a leap year")
+        else:
+            print("That year is'nt a leap year")
+    else:
+        print("That year is a leap year")
 else:
-    print("Your number is odd")
+    print("That year is'nt a leap year")
