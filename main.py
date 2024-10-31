d5428cfdedad2e091fbbb5b659561a06d0450f15
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-10-31 11:41:41 -0500

    Ejercicio 11: Numero par o impar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-C1 = float(input("Digit your first note: "))
-C2 = float(input("Digit your second note: "))
-Nl = float(input("Digit you laboratory note: "))
-Nc = (60 - Nl*0.3)/0.7
-C3 = (3 * Nc) - (C1 + C2)
-print(f"You need a {C3:.2f} on the tird note")
+num = int(input("Insert a number: "))
+if num % 2 == 0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
